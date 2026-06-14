c6bf106da889e7d7a59bbd0ae4bf46da7a9c98aa
exceedexpressid <exceedexpress.id@gmail.com>
2026-06-14 23:27:32 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'exceed-app-v1';
+const urlsToCache = [
+  '/',
+  '/manifest.json'
+];
+
+// Menginstal Service Worker dan menyimpan cache dasar
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Strategi Network First: Tarik data terbaru dari internet dulu, kalau sinyal mati baru pakai cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+// Membersihkan cache lama jika ada update aplikasi
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
